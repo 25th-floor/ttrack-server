dc19436c2cce27fb1aa4af349f01ab2b3b2163be
Thomas Subera <ts@25th-floor.com>
2017-07-07 16:02:49 +0200

    remove everything client webserver specific
    this is only an api server

--- src/server/app.js
+++ src/server/app.js
@@ -1,7 +1,6 @@
 /* eslint-disable import/no-dynamic-require*/
 const resources = require('./resources.js');
 const express = require('express');
-const cons = require('consolidate');
 const pg = require('pg');
 const raven = require('raven');
 
@@ -10,13 +9,6 @@ const app = express();
 const dbconfigfile = require(`${__dirname}/../../database.json`);
 const dbconfig = dbconfigfile.dev;
 
-// use NODE_ENV for defining which asset urls to use
-let assetsJS = 'http://localhost:3000/app.js';
-let assetsCSS = 'http://localhost:3000/app.css';
-if (process.env.NODE_ENV === 'production') {
-    assetsJS = '/app.js';
-    assetsCSS = '/app.css';
-}
 console.info('nodeENV', process.env.NODE_ENV);
 
 let buildInfo = {};
@@ -40,9 +32,6 @@ if (process.env.SENTRY_TOKEN) {
 
 console.info('Sentry', sentryClient ? 'enabled' : 'disabled');
 
-app.engine('html', cons.mustache);
-app.set('view engine', 'html');
-app.set('views', `${__dirname}/../../views`);
 app.set('sentry_client', sentryClient);
 
 app.set('pg', (fn) => {
@@ -58,15 +47,8 @@ app.set('pg', (fn) => {
 
 app.use('/api', resources.api);
 
-app.use(express.static(`${__dirname}/../../public`));
-
 app.all(/.*/, (req, res) => {
-    res.render('index', { css: assetsCSS, js: assetsJS, build: JSON.stringify(buildInfo) }, (err, html) => {
-        if (err) {
-            res.status(500).send('Internal Server Error').end();
-        }
-        res.send(html).end();
-    });
+    res.status(404).send('Not Found').end();
 });
 
 const port = process.env.PORT || 8080;
