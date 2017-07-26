15534383bb89019af35fdf3ecbce5740276c22d9
Thomas Subera <ts@25th-floor.com>
2017-07-26 16:55:45 +0200

    split period specifc code into it's own handler

--- src/api/handlers/user.js
+++ src/api/handlers/user.js
@@ -2,8 +2,7 @@
 const BaseJoi = require('joi')
     , Extension = require('joi-date-extensions')
     , User = require('../../resources/user')
-    , Timesheet = require('../../resources/timesheet')
-    , Period = require('../../resources/period');
+    , Timesheet = require('../../resources/timesheet');
 
 const Joi = BaseJoi.extend(Extension);
 
@@ -49,76 +48,6 @@ module.exports.timesheetFromToById = {
             .then(
                 (timesheet) => reply(timesheet),
                 error
-            );
-    }
-};
-
-module.exports.createPeriod = {
-    id: 'createPeriod',
-    validate: {
-        payload: Joi.object({
-            date: Joi.date().format('YYYY-MM-D').required(),
-            per_pty_id: Joi.string().required(),
-            //per_duration: Joi.number().integer().required(),
-            per_start: Joi.string().required(),
-            per_stop: Joi.string(),
-            per_break: Joi.string(),
-            per_comment: Joi.string(),
-        }),
-        params:{
-            userId: Joi.number().required(),
-        }
-    },
-    handler: function (request, reply){
-        const data = request.payload;
-        const { userId } = request.params;
-        console.info('API POST Request for Period for user', userId);
-        return Period
-            .post(userId, { ...data, userId })
-            .then(period => reply(period));
-    }
-};
-
-module.exports.updatePeriod = {
-    validate: {
-        payload: Joi.object({
-            date: Joi.date().format('YYYY-MM-DD').required(),
-            per_pty_id: Joi.string().required(),
-            per_start: Joi.string().required(),
-            per_stop: Joi.string(),
-            per_break: Joi.string(),
-            per_comment: Joi.string(),
-        }),
-        params:{
-            userId:  Joi.number().integer().required(),
-            per_id:  Joi.number().integer().required(),
-        }
-    },
-    handler: function (request, reply){
-        const data = request.payload;
-        const { userId, per_id } = request.params;
-        console.info(` API PUT Request for Period ${per_id} for user ${userId}`);
-        return Period
-            .put(userId, { ...data, userId, per_id })
-            .then(period => reply(period).code(201));
-    }
-};
-
-module.exports.deletePeriod = {
-    validate: {
-        params:{
-            userId:  Joi.number().integer().required(),
-            per_id:  Joi.number().integer().required(),
-        }
-    },
-    handler: function (request, reply){
-        const { userId, per_id } = request.params;
-        console.info(` API PUT DELETE for Period ${per_id} for user ${userId}`);
-        return Period
-            .delete(per_id, userId)
-            .then(
-                () => reply(),
-                error => console.error(error)
             );
     }
 };
--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,26 @@
 const Home = require('./handlers/home')
     , User = require('./handlers/user')
+    , Period = require('./handlers/period')
     , PeriodTypes = require('./handlers/periodTypes');
 
 exports.register = (plugin, options, next) => {
     plugin.route([
+        // Home
         { method: 'GET' , path: '/', config: Home.hello },
+
+        // User
         { method: 'GET' , path: '/users', config: User.list },
         { method: 'GET' , path: '/users/{id}', config: User.findById },
+
+        // Timesheet
         { method: 'GET' , path: '/users/{userId}/timesheet/{from}/{to}', config: User.timesheetFromToById },
-        
-        { method: 'POST', path: '/users/{userId}/periods', config: User.createPeriod },
-        { method: 'PUT' , path: '/users/{userId}/periods/{per_id}', config: User.updatePeriod },
-        { method: 'DELETE' , path: '/users/{userId}/periods/{per_id}', config: User.deletePeriod },
+
+        // Period
+        { method: 'POST', path: '/users/{userId}/periods', config: Period.create },
+        { method: 'PUT' , path: '/users/{userId}/periods/{per_id}', config: Period.update },
+        { method: 'DELETE' , path: '/users/{userId}/periods/{per_id}', config: Period.delete },
+
+        // PeriodTypes
         { method: 'GET' , path: '/period-types', config: PeriodTypes.list },
         
     ]);
