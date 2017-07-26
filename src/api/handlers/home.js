c1cb6f315fb9d78ffa478ed6072220c3ee134263
Thomas Subera <ts@25th-floor.com>
2017-07-26 16:55:26 +0200

    update welcome message
    + remove not used methods

--- src/api/handlers/home.js
+++ src/api/handlers/home.js
@@ -1,19 +1,6 @@
 module.exports.hello = {
     handler: function (request, reply) {
-        return reply({ result: 'Hello hapi!' });
+        return reply({ result: 'Hello this is the ttrack server api' });
     },
     id: 'root'
-};
-
-module.exports.restricted = {
-    auth: 'jwt',
-    handler: function (request, reply) {
-        return reply({ result: 'Restricted!' });
-    }
-};
-
-module.exports.notFound = {
-    handler: function (request, reply) {
-        return reply({ result: 'Oops, 404 Page!' }).code(404);
-    }
 };
