e39b3a58c9a2d1ac0155582b0026297a61b6b60c
Thomas Subera <ts@25th-floor.com>
2017-07-26 17:18:45 +0200

    minor adjustment of the user test

--- src/api/__test__/api-users.test.js
+++ src/api/__test__/api-users.test.js
@@ -1,13 +1,15 @@
-import Glue from 'glue'; 
+import Glue from 'glue';
 import path from 'path';
 import R from 'ramda';
 import { query, getClient } from '../../pg';
-import manifest from '../../config/manifest'; 
+import manifest from '../../config/manifest';
 
 const relativeTo = path.join(__dirname, '../../');
 const dummyUserSql = 'INSERT INTO users (usr_firstname , usr_lastname, usr_email) VALUES ( $1, $2, $3 ) RETURNING *';
 
-describe('API',() => {
+const apiUserPath = (user) => `/api/users/${user}`;
+
+describe('ttrack API', () => {
     let Server;
     beforeAll(async (done) => {
         Glue.compose(manifest, { relativeTo }, (err, server) => {
@@ -30,7 +32,7 @@ describe('API',() => {
     describe("Users", async () => {
         let user;
         beforeAll(async (done) => {
-            user = await query(dummyUserSql,['Mister', 'Smith', 'mister@smith.com']);
+            user = await query(dummyUserSql, ['Mister', 'Smith', 'mister@smith.com']);
             user = R.head(user.rows);
             done();
         });
@@ -40,7 +42,7 @@ describe('API',() => {
             done();
         });
 
-        describe('List', () => {
+        describe('Database Tests', () => {
             it('RAW SQL Get user with id x', async () => {
                 const result = [{
                     usr_id: user.usr_id,
@@ -48,70 +50,84 @@ describe('API',() => {
                     usr_email: 'mister@smith.com',
                     usr_firstname: 'Mister',
                     usr_employment_start: null,
-                    usr_employment_end: null 
+                    usr_employment_end: null
                 }];
-                const res = await query('SELECT * FROM users WHERE usr_id = $1',[user.usr_id]);
+                const res = await query('SELECT * FROM users WHERE usr_id = $1', [user.usr_id]);
                 expect(res.rows).toEqual(result);
             });
 
-            it('GET Api /api/users', async () => {
-                //let uri = Server.lookup('UserList').path;
-                const response  = await Server.inject({ method: 'GET', url: '/api/users', });
-                expect(response.statusCode).toBe(200);
-                expect(response.result).toBeArrayOfObjects();
-            });
-
-            it(`GET Api /api/users/{id}`, async () => {
-                const result = [{
-                    usr_id: user.usr_id,
-                    usr_lastname: 'Smith',
-                    usr_email: 'mister@smith.com',
-                    usr_firstname: 'Mister',
-                    usr_employment_start: null,
-                    usr_employment_end: null 
-                }];
-                const response  = await Server.inject({ method: 'GET', url: `/api/users/${user.usr_id}`});
-                expect(response.statusCode).toBe(200);
-                expect(response.result).toEqual(R.head(result));
-            });
-            
             // Should move into a own test
             describe('Create pg connection', async () => {
-                await it('test multiple querys on single connection', async () =>{
+                await it('test multiple querys on single connection', async () => {
                     const result = [{
                         usr_id: user.usr_id,
                         usr_lastname: 'Smith',
                         usr_email: 'mister@smith.com',
                         usr_firstname: 'Mister',
                         usr_employment_start: null,
-                        usr_employment_end: null 
+                        usr_employment_end: null
                     }];
                     const client = await getClient();
-                    const resultOne = await client.query('SELECT * FROM users WHERE usr_id = $1',[user.usr_id]);
-                    const resultTwo = await client.query('SELECT * FROM users WHERE usr_id = $1',[user.usr_id]);
+                    const resultOne = await client.query('SELECT * FROM users WHERE usr_id = $1', [user.usr_id]);
+                    const resultTwo = await client.query('SELECT * FROM users WHERE usr_id = $1', [user.usr_id]);
                     client.release();
                     expect(resultOne.rows).toEqual(result);
                     expect(resultTwo.rows).toEqual(result);
                 });
             });
 
-            it(`Methods POST `, async () => {
-                const POST = await Server.inject({ method: 'POST', url: `/api/users/${user.usr_id}`});
-                expect(POST.statusCode).toBe(405);
+        });
+
+        describe('testing GET requests', () => {
+            const userFixture = {
+                usr_lastname: 'Smith',
+                usr_email: 'mister@smith.com',
+                usr_firstname: 'Mister',
+                usr_employment_start: null,
+                usr_employment_end: null
+            };
+
+            it('GET Api /api/users returns our user', async () => {
+                //let uri = Server.lookup('UserList').path;
+                const response = await Server.inject({ method: 'GET', url: '/api/users', });
+                expect(response.statusCode).toBe(200);
+                expect(response.result).toBeArrayOfObjects();
+
+                expect(R.head(response.result)).toMatchObject({
+                    ...userFixture,
+                    usr_id: user.usr_id,
+                });
             });
 
-            it(`Methods PUT `, async () => {
-                const PUT = await Server.inject({ method: 'PUT', url: `/api/users/${user.usr_id}`});
+            it(`GET Api /api/users/{id}`, async () => {
+                const result = [{
+                    ...userFixture,
+                    usr_id: user.usr_id,
+                }];
+                const response = await Server.inject({ method: 'GET', url: `/api/users/${user.usr_id}` });
+                expect(response.statusCode).toBe(200);
+                expect(response.result).toEqual(R.head(result));
+            });
+        });
+
+        describe('Test Method not implemented', ()=>{
+            it(`Should fail method is not implemented POST on /api/users`, async ()=>{
+                const response  = await Server.inject({ method: 'POST' , url: '/api/users' });
+                expect(response.statusCode).toBe(405);
+            });
+
+            it(`Should fail method is not implemented PUT on ${apiUserPath('{user.usr_id}')}`, async () => {
+                const PUT = await Server.inject({ method: 'PUT', url: `/api/users/${user.usr_id}` });
                 expect(PUT.statusCode).toBe(405);
             });
 
-            it(`Methods DELETE`, async () => {
-                const DELETEmethod = await Server.inject({ method: 'DELETE', url: `/api/users/${user.usr_id}`});
+            it(`Should fail method is not implemented DELETE on ${apiUserPath('{user.usr_id}')}`, async () => {
+                const DELETEmethod = await Server.inject({ method: 'DELETE', url: `/api/users/${user.usr_id}` });
                 expect(DELETEmethod.statusCode).toBe(405);
             });
 
-            it(`Methods PATCH`, async () => {
-                const DELETEmethod = await Server.inject({ method: 'PATCH', url: `/api/users/${user.usr_id}`});
+            it(`Should fail method is not implemented PATCH on ${apiUserPath('{user.usr_id}')}`, async () => {
+                const DELETEmethod = await Server.inject({ method: 'PATCH', url: `/api/users/${user.usr_id}` });
                 expect(DELETEmethod.statusCode).toBe(405);
             });
 
