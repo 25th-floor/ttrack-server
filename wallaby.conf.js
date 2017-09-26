2b665a9df9bb6460296a023e6c9042708d7f6917
Philipp Bisson <pb@25th-floor.com>
2017-09-26 15:27:59 +0200

    Fix Wallaby Timezone
    postgress runs in docker and have diffrent TZ set

--- wallaby.conf.js
+++ wallaby.conf.js
@@ -1,6 +1,7 @@
 module.exports = function (wallaby) {
     process.env.DATABASE_URL = 'postgres://postgres:postgres@localhost:5432/ttrack_test';
     process.env.NODE_ENV = 'test';
+    process.env.TZ = 'UTC';
     return {
         files: [
             'src/**/*.js',
