32b8f15ff08cd66b1611b5c61d61fa4c6708d314
Philipp Bisson <pb@25th-floor.com>
2017-09-26 16:14:47 +0200

    Refactor to async await

--- src/resources/user.js
+++ src/resources/user.js
@@ -1,35 +1,32 @@
 const moment = require('moment')
-    , { query} = require('../pg');
+    , R = require('ramda')
+    , { query } = require('../pg');
 
 module.exports = {
-    list() {
+    async list() {
         const sql = 'SELECT * FROM users WHERE usr_employment_start IS NULL OR usr_employment_end IS NULL';
-        return query(sql)
-            .then(
-                result => result.rows,
-            );
+        const { rows } = await query(sql);
+        return rows;
     },
-    get(userId) {
+    async get(userId) {
         const sql = 'SELECT * FROM users WHERE usr_id = $1';
-        return query(sql, [userId])
-            .then(
-                result => result.rows[0],
-            );
+        const { rows } =  await query(sql, [userId])
+        return R.head(rows);
     },
     // get Users TargetTime for a specific date from the database
-    getTargetTime(userId, date) {
+    async getTargetTime(userId, date) {
         const sql = 'SELECT * FROM user_get_target_time($1, $2::DATE)';
-        return query(sql, [userId, date])
-            .then(
-                result => result.rows[0].user_get_target_time,
-            );
+        const { rows } = await query(sql, [userId, date]);
+        return R.head(rows).user_get_target_time;
     },
     // get Users TargetTime for a specific date from the database
-    getStartDate(userId) {
+    async getStartDate(userId) {
         const sql = 'SELECT * FROM user_get_start_date($1)';
-        return query(sql, [userId])
-            .then(
-                result => moment(result.rows[0].user_get_start_date),
-            );
+        const { rows } = await query(sql, [userId]);
+        return R.compose(
+            moment,
+            R.prop('user_get_start_date'),
+            R.head,
+        )(rows);
     },
 };
