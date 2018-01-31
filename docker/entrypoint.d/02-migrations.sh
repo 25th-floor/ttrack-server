099ce4e3d6d408ceb2496a3b64199c349cda5887
Thomas Subera <ts@25th-floor.com>
2018-01-31 20:45:06 +0100

    prepare production setup
    also add some logging and validate required parameters

--- docker/entrypoint.d/02-migrations.sh
+++ docker/entrypoint.d/02-migrations.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 set -e
+
+# Required parameters
+: "${DB_USER:?Must be set}"
+: "${DB_PASSWORD:?Must be set}"
+: "${DB_DATABASE:?Must be set}"
+: "${DB_HOST:?Must be set}"
+
+# Optional parameters with postgres defaults
+: "${DB_PORT:=5432}"
+: "${DB_DRIVER:=pg}"
+: "${DB_SCHEMA:=public}"
+
+
 echo "SETUP DATABASE CONFIG FROM DOCKERFILE/ENV"
 echo "#"
+echo "${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}/${DB_SCHEMA}"
 echo "#"
 printf '{
 	"sql-file" : true, 
@@ -22,8 +36,18 @@ printf '{
 		"host": "%s",
 		"driver": "%s", 
 		"schema": "%s"
+	},
+	"production": {
+		"user": "%s",
+		"password": "%s",
+		"database": "%s",
+		"port": "%s",
+		"host": "%s",
+		"driver": "%s",
+		"schema": "%s"
 	}
 }\n' \
+"$DB_USER" "$DB_PASSWORD" "$DB_DATABASE" "$DB_PORT" "$DB_HOST" "$DB_DRIVER" "$DB_SCHEMA" \
 "$DB_USER" "$DB_PASSWORD" "$DB_DATABASE" "$DB_PORT" "$DB_HOST" "$DB_DRIVER" "$DB_SCHEMA" \
 "$DB_USER" "$DB_PASSWORD" "$DB_DATABASE" "$DB_PORT" "$DB_HOST" "$DB_DRIVER" "$DB_SCHEMA" \
 > ./database.json
