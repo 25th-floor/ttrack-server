df6c4c0c32a65e7b8305caf88b706383c60656e4
Thomas Subera <ts@25th-floor.com>
2018-08-27 16:47:06 +0200

    fix home test

--- src/api/__test__/api-home.test.js
+++ src/api/__test__/api-home.test.js
@@ -1,14 +1,22 @@
 import { testServer } from './utils';
+const appRoot = require('app-root-path');
+const {
+    version, 
+    ttrackServer: {
+        apiVersion = 1,
+        validVersions = [1],
+    } 
+} = require(`${appRoot}/package.json`);
 
 describe('ttrack API', () => {
     describe('Home', async () => {
         it('should return the api version', async () => {
-            const {data, status} = await testServer.get('/');
+            const {data, status} = await testServer.get('/api/');
             expect(status).toEqual(200);
-            expect(data).toEqual({
-                "apiVersion": 1,
-                "validVersions": [1],
-                "version": "1.0.0"
+            expect(data).toMatchObject({
+                "apiVersion": apiVersion,
+                "validVersions": validVersions,
+                "version": version
             });
         });
     
